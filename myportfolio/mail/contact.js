fb259c1e978f515d610a07d194c8b9d84d9f4c17
mitmeerut007 <95173193+mitmeerut007@users.noreply.github.com>
2022-01-24 04:47:15 +0530

    Update contact.js

--- myportfolio/mail/contact.js
+++ myportfolio/mail/contact.js
@@ -35,10 +35,10 @@ $(function () {
                     $('#contactForm').trigger("reset");
                 },
                 error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success').html("<div class='alert alert-succes'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Thankyou " + name + ",I will contact you soon!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
